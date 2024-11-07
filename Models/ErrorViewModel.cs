6f9b6ba44795a1cced254aa5ddcb40fc1a3069d6
Joseph Gwara <54535857+JosephGwara@users.noreply.github.com>
2024-11-07 09:41:31 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV7112w_Practicum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
